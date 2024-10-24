54d4ce17ffcfdec67b5393b8fe86100b8081ae1c
muhammadma1ik <salmanmalik6812@outlook.com>
2024-10-24 20:19:55 +0400

    Add files via upload
    Used For Loops to create multiple loops with different parameters, then printed the outputs of the loops.

---
+++ 07-Some_Counting.py
@@ -0,0 +1,26 @@
+# This for Loop counts from 0 to 50 adding +1 with each iteration
+print("Counting up from 0 to 50 in increments of 1:")
+for i in range(0, 51):  
+    print(i)
+
+
+# This for Loop counts down from 50 to 0 decreasing by 1 with each iteration
+print("\nCounting down from 50 to 0 in decrements of 1:")
+for i in range(50, -1, -1): 
+    print(i)
+
+
+# This for Loop counts from 30 to 50 adding +1 with each iteration
+print("\nCounting up from 30 to 50 in increments of 1:")
+for i in range(30, 51): 
+    print(i)
+
+# This for Loop counts down 50 to 10, decreasing by 2 with each iteration
+print("\nCounting down from 50 to 10 in decrements of 2:")
+for i in range(50, 9, -2):  
+    print(i)
+
+# This for Loop counts from 100 to 200 adding +1 with each iteration
+print("\nCounting up from 100 to 200 in increments of 5:")
+for i in range(100, 201, 5):
+    print(i)
