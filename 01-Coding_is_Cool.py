3f75349d38651a9a0060f4eea662cfc5e71125f8
muhammadma1ik <salmanmalik6812@outlook.com>
2024-10-24 20:06:51 +0400

    Add files via upload
    Used String concatenation to combine the variables and print the phrase

---
+++ 01-Coding_is_Cool.py
@@ -0,0 +1,6 @@
+word1 = "Coding"
+word2 = "is"
+word3 = "Cool"
+
+# Using string concatenation to concatenate the variables and the spaces in between them
+print('"' + word1 + " " + word2 + " " + word3 + '"')
