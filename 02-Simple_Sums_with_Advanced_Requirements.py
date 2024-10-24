a7b36574177c9b34e65d9f3f86d696987c1319bb
muhammadma1ik <salmanmalik6812@outlook.com>
2024-10-24 20:08:18 +0400

    Add files via upload
    Created a program that declares variables, sums the variables and then prints it to the console

---
+++ 02-Simple_Sums_with_Advanced_Requirements.py
@@ -0,0 +1,11 @@
+#Declaring the first variable with the integer value of 8
+num1 = 8
+
+#Declaring the second variable with the integer value of 10
+num2 = 10
+
+#Declaring another variable that stores the sum of the first two numbers
+sum = num1 + num2
+
+#printing the value stored in the sum variable
+print(f"The Sum of {num1} and {num2} is {sum}")
